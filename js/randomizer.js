00c0a737a03ad5f06400e0f171e84b19cefbfcd9
Roman Gotovko <gotovkoromanhzby@gmail.com>
2018-06-30 22:33:38 +0300

    ...

--- js/randomizer.js
+++ js/randomizer.js
@@ -17,181 +17,181 @@ function redraw(cardCount) {
         {
             "id": 1,
             "title": "Попробуй убей!",
-            "heroes": ["Cho", "Gall", "Alexstrasza", "Lt. Morales", "Auriel"],
+            "heroes": ["Чо ", "Галл ", "Алекстраза ", "Лт. Моралес ", "Ауриэль "],
             "img": "img/cards/01.png"
         },
         {
             "id": 2,
             "title": "Глобальный хаос",
-            "heroes": ["Brightwing", "The Lost Vikings", "Abathur", "Falstad", "Dehaka"],
+            "heroes": ["Светик ", "Потерявшиеся Викинги ", "Абатур ", "Фалстад ", "Дехака "],
             "img": "img/cards/02.png"
         },
         {
             "id": 3,
             "title": "Орк пати",
-            "heroes": ["Garrosh", "Samuro", "Thrall", "Rehgar", "Gul'dan"],
+            "heroes": ["Гаррош ", "Самуро ", "Тралл ", "Регар ", "Гул'дан "],
             "img": "img/cards/03.png"
         },
         {
             "id": 4,
             "title": "Свежее мясо!",
-            "heroes": ["The Lost Vikings", "Murky", "Rexxar", "D.Va", "The Butcher"],
+            "heroes": ["Потерявшиеся Викинги ", "Мурчаль ", "Рексар ", "D.Va ", "Мясник "],
             "img": "img/cards/04.png"
         },
         {
             "id": 5,
             "title": "Жмурки",
-            "heroes": ["Nova", "Valeera", "Zeratul", "Samuro", "Tyrande"],
+            "heroes": ["Нова ", "Валира ", "Зератул ", "Самуро ", "Тиранда "],
             "img": "img/cards/05.png"
         },
         {
             "id": 6,
             "title": "Тэрибл дэмедж",
-            "heroes": ["Johanna", "Tyrael", "E.T.C.", "Anub'arak", "Lt. Morales"],
+            "heroes": ["Джоанна ", "Тираэль ", "E.T.C.", "Ануб'арак ", "Лт. Моралес "],
             "img": "img/cards/06.png"
         },
         {
             "id": 7,
             "title": "Мастера Меча",
-            "heroes": ["Varian", "Samuro", "Arthas", "Sonya", "Tyrael"],
+            "heroes": ["Вариан ", "Самуро ", "Артас ", "Соня ", "Тираэль "],
             "img": "img/cards/07.png"
         },
         {
             "id": 8,
             "title": "Чип и Дейл",
-            "heroes": ["Cho", "Gall", "Abathur", "The Lost Vikings", "Murky"],
+            "heroes": ["Чо ", "Галл ", "Абатур ", "Потерявшиеся Викинги ", "Мурчаль "],
             "img": "img/cards/08.png"
         },
         {
             "id": 9,
             "title": "Прыг-скок",
-            "heroes": ["Lunara", "Li-Ming", "Tracer", "Illidan", "Genji"],
+            "heroes": ["Лунара ", "Ли-Мин ", "Трейсер ", "Иллидан ", "Гензи "],
             "img": "img/cards/09.png"
         },
         {
             "id": 10,
             "title": "Толстый доспех = успех",
-            "heroes": ["Blaze", "Johanna", "Varian", "Artanis", "Arthas"],
+            "heroes": ["Блэйз ", "Джоанна ", "Вариан ", "Артанис ", "Артас "],
             "img": "img/cards/10.png"
         },
         {
             "id": 11,
             "title": "Что ты такое?",
-            "heroes": ["E.T.C.", "Stitches", "Abathur", "Cho", "Gall"],
+            "heroes": ["E.T.C.", "Стежок ", "Абатур ", "Чо ", "Галл "],
             "img": "img/cards/11.png"
         },
         {
             "id": 12,
             "title": "Киборги заполонили всю планету",
-            "heroes": ["Genji", "Probius", "Fenix", "Blaze", "D.Va"],
+            "heroes": ["Гензи ", "Пробиус ", "Феникс ", "Блэйз ", "D.Va "],
             "img": "img/cards/12.png"
         },
         {
             "id": 13,
             "title": "Секси Чики",
-            "heroes": ["Tyrande", "Kerrigan", "Valeera", "Sylvanas", "Alexstrasza"],
+            "heroes": ["Тиранда ", "Керриган ", "Валира ", "Сильвана ", "Алекстраза "],
             "img": "img/cards/13.png"
         },
         {
             "id": 14,
             "title": "Несите патроны!",
-            "heroes": ["Raynor", "Tychus", "Tracer", "Ana", "Nova"],
+            "heroes": ["Рейнор ", "Тайкус ", "Трейсер ", "Ана ", "Нова "],
             "img": "img/cards/14.png"
         },
         {
             "id": 15,
             "title": "ЧСВ – наше всё",
-            "heroes": ["Ragnaros", "Alarak", "Varian", "Hanzo", "Valeera"],
+            "heroes": ["Рагнарос ", "Аларак ", "Вариан ", "Хандзо ", "Валира "],
             "img": "img/cards/15.png"
         },
         {
             "id": 16,
             "title": "Сильное колдунство",
-            "heroes": ["Kael'thas", "Kel'Thuzad", "Jaina", "Li-Ming", "Chromie"],
+            "heroes": ["Кель'тас ", "Кел'Тузад ", "Джайна ", "Ли-Мин ", "Хроми "],
             "img": "img/cards/16.png"
         },
         {
             "id": 17,
             "title": "Малявки",
-            "heroes": ["Chromie", "The Lost Vikings", "Probius", "Murky", "Brightwing"],
+            "heroes": ["Хроми ", "Потерявшиеся Викинги ", "Пробиус ", "Мурчаль ", "Светик "],
             "img": "img/cards/17.png"
         },
         {
             "id": 18,
             "title": "Зерго-туса",
-            "heroes": ["Abathur", "Dehaka", "Stukov", "Zagara", "Kerrigan"],
+            "heroes": ["Абатур ", "Дехака ", "Стуков ", "Загара ", "Керриган "],
             "img": "img/cards/18.png"
         },
         {
             "id": 19,
             "title": "Протосс пати",
-            "heroes": ["Tassadar", "Fenix", "Artanis", "Zeratul", "Probius"],
+            "heroes": ["Тассадар ", "Феникс ", "Артанис ", "Зератул ", "Пробиус "],
             "img": "img/cards/19.png"
         },
         {
             "id": 20,
             "title": "Власть огня",
-            "heroes": ["Ragnaros", "Blaze", "Alexstrasza", "Kael'thas", "Junkrat"],
+            "heroes": ["Рагнарос ", "Блэйз ", "Алекстраза ", "Кель'тас ", "Крысавчик "],
             "img": "img/cards/20.png"
         },
         {
             "id": 21,
             "title": "Форпост",
-            "heroes": ["Probius", "Gazlowe", "Sgt. Hammer", "Blaze", "Deckard"],
+            "heroes": ["Пробиус ", "Газлоу ", "Срж. Кувалда ", "Блэйз ", "Декард "],
             "img": "img/cards/21.png"
         },
         {
             "id": 22,
             "title": "Свет опалит тебя!",
-            "heroes": ["Johanna", "Yrel", "Auriel", "Tyrael", "Uther"],
+            "heroes": ["Джоанна ", "Ирель ", "Ауриэль ", "Тираэль ", "Утер "],
             "img": "img/cards/22.png"
         },
         {
             "id": 23,
             "title": "Нас не пробить!",
-            "heroes": ["Artanis", "Chen", "Tassadar", "Zarya", "Medivh"],
+            "heroes": ["Артанис ", "Чэнь ", "Тассадар ", "Заря ", "Медив "],
             "img": "img/cards/23.png"
         },
         {
             "id": 24,
             "title": "Мы тебя везде достанем!",
-            "heroes": ["Tyrande", "Artanis", "Falstad", "Abathur", "Hanzo"],
+            "heroes": ["Тиранда ", "Артанис ", "Фалстад ", "Абатур ", "Хандзо "],
             "img": "img/cards/24.png"
         },
         {
             "id": 25,
             "title": "Тёмные личности",
-            "heroes": ["Diablo", "Xul", "Leoric", "Malthael", "Greymane"],
+            "heroes": ["Диабло ", "Зул ", "Леорик ", "Малтаэль ", "Седогрив "],
             "img": "img/cards/25.png"
         },
         {
             "id": 26,
             "title": "Diablo-туса",
-            "heroes": ["Azmodan", "Kharazim", "Cassia", "Nazeebo", "Valla"],
+            "heroes": ["Азмодан ", "Каразим ", "Кассия ", "Назибо ", "Валла "],
             "img": "img/cards/26.png"
         },
         {
             "id": 27,
             "title": "Хилимся-живём",
-            "heroes": ["Zul'jin", "Li Li", "Lúcio", "Malfurion", "Ana"],
+            "heroes": ["Зул'джин ", "Ли Ли ", "Лусио ", "Малфурион ", "Ана "],
             "img": "img/cards/27.png"
         },
         {
             "id": 28,
             "title": "Тяни/толкай",
-            "heroes": ["Maiev", "Stukov", "Alarak", "Kel'Thuzad", "Yrel"],
+            "heroes": ["Майев ", "Стуков ", "Аларак ", "Кел'Тузад ", "Ирель "],
             "img": "img/cards/28.png"
         },
         {
             "id": 29,
             "title": "Кто хочет подраться?",
-            "heroes": ["Sonya", "Muradin", "Illidan", "Garrosh", "Diablo"],
+            "heroes": ["Соня ", "Мурадин ", "Иллидан ", "Гаррош ", "Диабло "],
             "img": "img/cards/29.png"
         },
         {
             "id": 30,
             "title": "Нам лень ходить",
-            "heroes": ["Tyrael", "Malthael", "Sgt. Hammer", "Auriel", "Falstad"],
+            "heroes": ["Тираэль ", "Малтаэль ", "Срж. Кувалда ", "Ауриэль ", "Фалстад "],
             "img": "img/cards/30.png"
         }
     ];
